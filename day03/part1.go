dd57eb268409e9ab6a5ad589bad66bfce0a76a0c
BenJuan26 <benjamin.schubert@qlik.com>
2021-12-07 22:08:44 -0500

    Day 3 part2

--- day03/part1.go
+++ day03/part1.go
@@ -7,16 +7,9 @@ import (
 	advent "github.com/BenJuan26/advent2021"
 )
 
-func Part1() {
-	lines, err := advent.ReadInput()
-	if err != nil {
-		panic(err)
-	}
+func countBits(lines []string) (cols int, zeroes, ones []int) {
+	cols = len(lines[0])
 
-	cols := len(lines[0])
-
-	zeroes := []int{}
-	ones := []int{}
 	for i := 0; i < cols; i++ {
 		zeroes = append(zeroes, 0)
 		ones = append(ones, 0)
@@ -32,14 +25,52 @@ func Part1() {
 		}
 	}
 
-	gamma := 0
+	return cols, zeroes, ones
+}
+
+func getGamma(cols int, zeroes, ones []int) (int, string) {
+	gammaI := 0
+	gammaS := ""
+
 	for i := 0; i < cols; i++ {
-		if ones[i] > zeroes[i] {
-			gamma += int(math.Pow(2, float64(cols-i-1)))
+		if ones[i] >= zeroes[i] {
+			gammaI += int(math.Pow(2, float64(cols-i-1)))
+			gammaS += "1"
+		} else {
+			gammaS += "0"
 		}
 	}
 
-	epsilon := int(math.Pow(2, float64(cols))) - gamma - 1
+	return gammaI, gammaS
+}
+
+func getEpsilon(cols int, zeroes, ones []int) (int, string) {
+	epI := 0
+	epS := ""
+
+	for i := 0; i < cols; i++ {
+		if ones[i] < zeroes[i] {
+			epI += int(math.Pow(2, float64(cols-i-1)))
+			epS += "1"
+		} else {
+			epS += "0"
+		}
+	}
+
+	return epI, epS
+}
+
+func Part1() {
+	lines, err := advent.ReadInput()
+	if err != nil {
+		panic(err)
+	}
+
+	cols, zeroes, ones := countBits(lines)
+	gamma, _ := getGamma(cols, zeroes, ones)
+
+	// epsilon := int(math.Pow(2, float64(cols))) - gamma - 1
+	epsilon, _ := getEpsilon(cols, zeroes, ones)
 	fmt.Println(gamma * epsilon)
 }
 
--- day03/part2.go
+++ day03/part2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	advent "github.com/BenJuan26/advent2021"
 )
@@ -12,5 +13,59 @@ func Part2() {
 		panic(err)
 	}
 
-	fmt.Println(lines[0])
+	cols := len(lines[0])
+	col := 0
+	for len(lines) > 1 {
+		_, zeroes, ones := countBits(lines)
+		_, gamma := getGamma(cols, zeroes, ones)
+
+		newLines := []string{}
+		for _, line := range lines {
+			if line[col] == gamma[col] {
+				newLines = append(newLines, line)
+			}
+		}
+
+		lines = newLines
+		col += 1
+	}
+
+	o2S := lines[0]
+	o2I := 0
+	for i := 0; i < cols; i++ {
+		if o2S[i] == "1"[0] {
+			o2I += int(math.Pow(2, float64(cols-i-1)))
+		}
+	}
+
+	lines, err = advent.ReadInput()
+	if err != nil {
+		panic(err)
+	}
+
+	col = 0
+	for len(lines) > 1 {
+		_, zeroes, ones := countBits(lines)
+		_, ep := getEpsilon(cols, zeroes, ones)
+
+		newLines := []string{}
+		for _, line := range lines {
+			if line[col] == ep[col] {
+				newLines = append(newLines, line)
+			}
+		}
+
+		lines = newLines
+		col += 1
+	}
+
+	co2S := lines[0]
+	co2I := 0
+	for i := 0; i < cols; i++ {
+		if co2S[i] == "1"[0] {
+			co2I += int(math.Pow(2, float64(cols-i-1)))
+		}
+	}
+
+	fmt.Println(co2I * o2I)
 }
