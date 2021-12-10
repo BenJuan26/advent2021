2a95b0114e7c54bac9a78e754429871e13c97f67
BenJuan26 <benjamin.schubert@qlik.com>
2021-12-10 18:38:49 -0500

    Day 7 part2

--- day07/part2.go
+++ day07/part2.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
 
 	advent "github.com/BenJuan26/advent2021"
 )
+
+func SeriesSum(n int) int {
+	return int(n * (2 + (n - 1)) / 2)
+}
 
 func Part2() {
 	lines, err := advent.ReadInput()
@@ -12,5 +19,24 @@ func Part2() {
 		panic(err)
 	}
 
-	fmt.Println(lines[0])
+	posStrings := strings.Split(lines[0], ",")
+	positions := []int{}
+	runningTotal := 0
+	for _, posString := range posStrings {
+		pos, err := strconv.Atoi(posString)
+		if err != nil {
+			panic(err)
+		}
+		positions = append(positions, pos)
+		runningTotal += pos
+	}
+
+	avg := int(math.Round(float64(runningTotal / len(positions))))
+
+	total := 0
+	for _, pos := range positions {
+		total += SeriesSum(int(math.Abs(float64(pos - avg))))
+	}
+
+	fmt.Println(total)
 }
